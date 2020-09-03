db0dd009132dadbad77c00fc732820e168bb5975
gpofblank <metodiev.design@gmail.com>
2020-09-03 17:55:37 +0300

    basic navigation between main and admin - done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./main/auth/pages/login-page/login-page.module').then(m => m.LoginPageModule)},
+  { path: 'admin', loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)},
+  { path: 'main', loadChildren: () => import('./main/main-routing.module').then(m => m.MainRoutingModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
